745e91279d331e868c5f8663cba2a3ac97837e24
FelipeGXavier <felipexavier20015@gmail.com>
2019-09-07 23:18:23 -0300

    Operações de instanciação

--- src/main/java/com/felipe/xavier/SpringmcApplication.java
+++ src/main/java/com/felipe/xavier/SpringmcApplication.java
@@ -1,13 +1,31 @@
 package com.felipe.xavier;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.felipe.xavier.domain.Categoria;
+import com.felipe.xavier.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringmcApplication {
+public class SpringmcApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmcApplication.class, args);
+	}
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null, "Teste 1 ");
+		Categoria c2 = new Categoria(null, "Teste 2 ");
+		categoriaRepository.saveAll(Arrays.asList(c1, c2));
+		
 	}
 
 }
